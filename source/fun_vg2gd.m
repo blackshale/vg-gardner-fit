035010adf0bf6b140ec40f6c51f011544c9341dc
blackshale <jjabssil1127@gmail.com>
2022-05-05 15:38:00 +0900

    completed

---
+++ source/fun_vg2gd.m
@@ -0,0 +1,15 @@
+function [ag,theta,kr] = fun_vg2gd(av,n,theta_s,theta_r,psi)
+
+
+% ----linear descrete GD model
+m = 1 - 1/n;
+THET = (1 + (av*(abs(psi))).^n).^(-m);
+kr_vg = THET.^0.5.*(1 - (1 - THET.^(1/m)).^m).^2;
+theta = theta_r + THET*(theta_s - theta_r);
+
+
+ag = log(kr_vg)./psi;
+kr = exp(ag*psi);
+
+end
+
